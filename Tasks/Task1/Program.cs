ed83c9d0d1be6f3496d1f9b17b814df871f15745
kovalev70 <kovalev85291@gmail.com>
2024-02-05 23:26:54 +0700

    Task1

---
+++ Tasks/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите сумму вклада:");
+
+        if (double.TryParse(Console.ReadLine(), out double depositAmount))
+        {
+            double interestRate = 0;
+
+            if (depositAmount < 100)
+            {
+                interestRate = 0.05;
+            }
+            else if (depositAmount >= 100 && depositAmount <= 200)
+            {
+                interestRate = 0.07;
+            }
+            else
+            {
+                interestRate = 0.10;
+            }
+
+            double totalAmount = depositAmount * (1 + interestRate);
+
+            Console.WriteLine($"Сумма вклада с начисленными процентами: {totalAmount}");
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода. Пожалуйста, введите корректное число для суммы вклада.");
+        }
+    }
+}
