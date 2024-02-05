cf659d0a93a5cb6b67a6e46e95dad1c17c1fa8b7
kovalev70 <kovalev85291@gmail.com>
2024-02-05 23:52:13 +0700

    Task3

---
+++ Tasks/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] array = { 6, 7, 2, 3, 9, 4, 7, 1, 3, 6, 8, 3, 7, 8, 3 };
+        int minSum = int.MaxValue;
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            int currentSum = array[i] + array[i + 1];
+
+            if (currentSum < minSum)
+            {
+                minSum = currentSum;
+            }
+        }
+
+        Console.WriteLine($"Минимальная сумма: {minSum}");
+    }
+}
