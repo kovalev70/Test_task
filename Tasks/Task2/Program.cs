907133954b5c222727b47f753827f68b532bbed8
kovalev70 <kovalev85291@gmail.com>
2024-02-10 15:40:52 +0700

    Task2 Refactoring

--- Tasks/Task2/Program.cs
+++ Tasks/Task2/Program.cs
@@ -1,38 +1,22 @@
-﻿class Program
+﻿
+using System;
+using System.Linq;
+
+class Program
 {
     static void Main()
     {
         int[,,] array = { { { 1, 2 }, { 3, 4 } },
-                        { { 4, 5 }, { 6, 7 } },
-                        { { 7, 8 }, { 9, 10 } },
-                        { { 10, 11 }, { 12, 13 } }
-                      };
-
-        int firstDimensionLength = array.GetLength(0);
-        int secondDimensionLength = array.GetLength(1);
-        int thirdDimensionLength = array.GetLength(2);
+                          { { 4, 5 }, { 6, 7 } },
+                          { { 7, 8 }, { 9, 10 } },
+                          { { 10, 11 }, { 12, 13 } }
+                        };
 
         Console.Write("{");
-        for (int i = 0; i < firstDimensionLength; i++)
-        {
-            Console.Write("{");
-            for (int j = 0; j < secondDimensionLength; j++)
-            {
-                Console.Write("{");
-                for (int k = 0; k < thirdDimensionLength; k++)
-                {
-                    Console.Write(array[i, j, k]);
-                    if (k < thirdDimensionLength - 1)
-                        Console.Write(" , ");
-                }
-                Console.Write("}");
-                if (j < secondDimensionLength - 1)
-                    Console.Write(" , ");
-            }
-            Console.Write("}");
-            if (i < firstDimensionLength - 1)
-                Console.Write(" , ");
-        }
+        Console.Write(string.Join(" , ", Enumerable.Range(0, array.GetLength(0))
+            .Select(i => "{" + string.Join(" , ", Enumerable.Range(0, array.GetLength(1))
+                .Select(j => "{" + string.Join(" , ", Enumerable.Range(0, array.GetLength(2))
+                    .Select(k => array[i, j, k])) + "}")) + "}")));
         Console.Write("}");
     }
 }
